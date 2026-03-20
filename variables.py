22d04454431919e226a2a800c8c92c7026e09533
Devarapalli Jahnavi Durga Indumathi <indumathidevarapalli12@gmail.com>
2026-03-20 12:48:07 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,105 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+a=10
+print(a)
+10
+a=6
+b=2
+print(a+b)
+8
+c=30
+print(C)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(C)
+NameError: name 'C' is not defined. Did you mean: 'c'?
+print(c)
+30
+d=10
+print(e)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    print(e)
+NameError: name 'e' is not defined
+print(d)
+10
+2=6
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a0123456789=100
+print(a0123456789)
+100
+name="Indu"
+print(name)
+Indu
+city="Vijayawada"
+print(city)
+Vijayawada
+course="python"
+print(course)
+python
+
+if=100
+SyntaxError: invalid syntax
+elif=20
+SyntaxError: invalid syntax
+6=100
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a=2,b=3
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=2;b=4
+print(a+b)
+6
+c=6
+print(c)
+6
+a,b="python","course"
+print(a+b)
+pythoncourse
+print(a+" "+b)
+python course
+>>> fname="Indumathi"
+>>> lname="D"
+>>> print(fname+lname)
+IndumathiD
+>>> print(fname+" "+lname)
+Indumathi D
+>>> 
+>>> @a=10
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> _a=100
+>>> print(_a)
+100
+>>> _=20
+>>> print(_)
+20
+>>>  a=40
+...  
+SyntaxError: unexpected indent
+>>> first name="Indu"
+SyntaxError: invalid syntax
+>>> firstname="Indu"
+>>> print(firstname)
+Indu
+>>> first_name="Indu"
+>>> print(first_name)
+Indu
+>>> 
+>>> a=4
+>>> print(a)
+4
+>>> del a
+>>> a
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    a
+NameError: name 'a' is not defined. Did you mean: '_a'?
+>>> NAME="Indu"
+>>> print(NAME)
+Indu
+>>> name="Indu"
+>>> print(name)
+Indu
+>>> Name="Indu"
+>>> print(Name)
+Indu
