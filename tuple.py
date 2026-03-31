7fc6d640f9e2536aaf940e2a31da644fe43f2e97
Devarapalli Jahnavi Durga Indumathi <indumathidevarapalli12@gmail.com>
2026-03-31 20:57:56 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,12 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #tuples()
+>>> a=(5,8.9,"python",4+9j,True,False)
+>>> type(a)
+<class 'tuple'>
+>>> len(a)
+6
+>>> a.count(True)
+1
+>>> a.index("python")
+2
