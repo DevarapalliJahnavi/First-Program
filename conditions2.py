8371f7f398b37461c68ae362dc6c0297e6a6b2c0
Devarapalli Jahnavi Durga Indumathi <indumathidevarapalli12@gmail.com>
2026-04-04 19:32:28 +0530

    Add files via upload

---
+++ conditions2.py
@@ -0,0 +1,244 @@
+#if-else conditions by using comparision
+'''a=4
+b=9
+if a<b:
+    print("less")
+else:
+    print("true")'''
+
+'''a=4
+b=9
+if a>b:
+    print("less")
+else:
+    print("true")'''
+
+'''a=4
+b=9
+if a<b:
+    print("less")
+else:
+    print("true")'''
+
+#conditions by using logical
+'''a=3
+b=7
+if a<b and a>b:
+    print("less")
+else:
+    print("true")'''
+
+'''a=3
+b=7
+if a<b or a>b:
+    print("less")
+else:
+    print("true")'''
+    
+'''a=3
+b=7
+if a<b not b>a:
+    print("less")
+else:
+    print("true")'''
+
+#identify
+'''a=4
+if type(a) is int:
+    print("true")
+else:
+    print("less")'''
+
+
+#if-elif conditions by using comparision
+'''a=2
+b=4
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")'''
+
+'''a=2
+b=4
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")'''
+
+'''a=2
+b=4
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+'''a=2
+b=4
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+#if-elif-else conditions
+'''a=5
+b=7
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=5
+b=7
+if a>b:
+    print("less")
+elif b>a:
+    print("greater")
+else a!=b:
+    print("true")'''
+
+'''a=5
+b=7
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=5
+b=7
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+elif a!=b:
+    print("not equal")
+else:
+    print("true")'''
+
+#logical
+'''a=3
+b=6
+if a<b and a>b:
+    print("less")
+elif a>b and a<b:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=3
+b=6
+if a>b or a<b:
+    print("less")
+elif a<b or a>b:
+    print("greater")
+else:
+    print("true")'''
+
+#identify
+'''a=5
+if type(a) is int:
+    print("true")
+elif a>b and a<b:
+    print("greater")
+else:
+    print("true")'''
+
+
+
+#multiple-if conditions
+'''a=8
+b=12
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+'''a=8
+b=12
+if a==b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+'''a=8
+b=12
+if a==b:
+    print("less")
+if b<a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+#nested-if conditions
+'''a=10
+b=20
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=10
+b=20
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=10
+b=20
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")'''
+
+'''a=10
+b=20
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")
+    else:
+         print("true")'''
+
+'''a=10
+b=20
+if a==b:
+    print("less")
+    if b>a:
+        print("greater")
+else:
+    print("false")'''
+
+'''a=10
+b=20
+if a==b:
+    print("less")
+    if b<a:
+        print("greater")
+    else:
+         print("true")
+else:
+        print("false")'''
+
+'''a=6
+b=9
+if a!=b:
+    print("not equal")
+    if b<a:
+        print("less")
+    elif a>b:
+        print("true")
+    else:
+         print("false")'''
+
