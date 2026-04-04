0b13720cc8de58457d1f39d16706971719e49e98
Devarapalli Jahnavi Durga Indumathi <indumathidevarapalli12@gmail.com>
2026-04-04 19:33:04 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,151 @@
+#for loop
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=",")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+#print(type(a))'''
+
+#tuple
+'''a=(5,6,7,8,9,10,11)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#set
+'''a={4,5,6,7,8}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#dictionary
+'''a={"name":"indu","year":2026,"month":4}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.keys():
+    print(i)
+for i in a.values():
+    print(type(a))
+    print(type(i))
+    print(i)
+for i in a.items():
+    print(type(a))
+    print(type(i))
+    print(i)'''
+
+#string
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+'''a=["codegnan","python","course"]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#float
+'''a=[2.4,3.6,4.8]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#complex
+'''a=[2+4j,3+6j,4+8j]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#boolean
+'''a=[True,False]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''upper=["python","java","ds"]
+for i in upper:
+    print(i.upper(),end=",")'''
+
+
+#while loop is continuous iteration
+'''a=10
+while a<1:
+    print("true")'''
+
+'''a=10
+while a>1:
+    print("true")'''
+
+'''a=10
+while a>=1:
+    print(a)
+    a=a-1'''
+
+'''a=10
+while a>1:
+      a=a-1
+      print(a)'''
+
+'''a=15
+while a>2:
+    print(a)
+    a-=1'''
+
+'''a=3
+while a<20:
+    print(a)
+    a+=1'''
+
+'''while True:
+    age=int(input("enter the age"))
+    if age>=18:
+        print("eligible for vote")
+    else:
+        print("not eligible for vote")'''
+
+'''while True:
+    num=int(input("enter the number"))
+if num%2==0: 
+    print("it is even ")
+else:
+    print("it is odd")'''
+
+#Range : The range function returns a sequences of numbers ,starting from 0 by default and increments by 1 i by an it stops before a specified number. 
+#start-stop-step
+'''for i in range(10):
+    print(i)'''
+
+'''for i in range(15,36):
+  print(i)'''
+
+'''for i in range(19):
+  print(i)'''
+
+'''for i in range(2,20,2):
+    print(i)'''
+
+'''for i in range(0,30,2):
+    print(i)'''
+
+'''for i in range(5,50,5):
+    print(i)'''
+    
